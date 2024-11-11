d8f958b8a0d45be7f2654f8e9f82e654564d82c0
Cristian Nass <cristian.tirdad@gmail.com>
2024-11-11 02:24:42 +0100

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
-
+import NavigationBar from "./components/NavigationBar";
 function App() {
   return (
     <>
+      <NavigationBar />
       <div>
         <p className='read-the-docs'>Wellcome to the app</p>
       </div>
