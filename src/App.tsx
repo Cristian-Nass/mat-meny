257e5fd9e56b493bf021f3c13c2c0ac23eff5724
Cristian Nass <cristian.tirdad@gmail.com>
2024-11-11 01:49:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className='read-the-docs'>Wellcome to the app</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
